809e67fe9bfd30ae1aaa55a7adb3c6e5be552c3b
Piruthuviraj <piruthuviraj22@gmail.com>
2024-04-23 20:11:19 +0530

    dashboard page

--- boardo-admin/src/views/dashboard/Dashboard.tsx
+++ boardo-admin/src/views/dashboard/Dashboard.tsx
@@ -1,7 +1,20 @@
+import { Box, Button, Paper, TextField } from "@mui/material";
+import { TextField2 } from "components/ui-component/customizedComponents";
+
 export default function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <Paper
+      elevation={1}
+      sx={{
+        width: "600px",
+        marginTop: "100px",
+      }}
+    >
+      <Box p={2}>
+        <TextField2 label="Place Name" />
+        <TextField />
+        <Button>Submit</Button>
+      </Box>
+    </Paper>
   );
 }
