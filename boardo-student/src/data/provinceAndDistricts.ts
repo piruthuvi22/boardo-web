02e41e3575cd3bdf2f06e5988739a25ecf1975a0
Piruthuviraj <piruthuviraj22@gmail.com>
2024-04-27 02:47:39 +0530

    commit

---
+++ boardo-student/src/data/provinceAndDistricts.ts
@@ -0,0 +1,44 @@
+interface ProvincesAndDistricts {
+    province: string;
+    districts: string[];
+  }
+  
+  export const PROVINCES_AND_DISTRICTS: ProvincesAndDistricts[] = [
+    {
+      province: "Northern",
+      districts: ["Jaffna", "Kilinochchi", "Mannar", "Mullaitivu", "Vavuniya"],
+    },
+    {
+      province: "North Western",
+      districts: ["Kurunegala", "Puttalam"],
+    },
+    {
+      province: "Western",
+      districts: ["Gampaha", "Colombo", "Kalutara"],
+    },
+    {
+      province: "North Central",
+      districts: ["Anuradhapura", "Polonnaruwa"],
+    },
+    {
+      province: "Central",
+      districts: ["Kandy", "Matale", "Nuwara Eliya"],
+    },
+    {
+      province: "Sabaragamuwa",
+      districts: ["Kegalle", "Ratnapura"],
+    },
+    {
+      province: "Eastern",
+      districts: ["Trincomalee", "Batticaloa", "Ampara"],
+    },
+    {
+      province: "Southern",
+      districts: ["Galle", "Matara", "Hambantota"],
+    },
+    {
+      province: "Uva",
+      districts: ["Badulla", "Monaragala"],
+    },
+  ];
+  
--- boardo-student/src/views/profile/Profile.tsx
+++ boardo-student/src/views/profile/Profile.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
+  Autocomplete,
+  Avatar,
   Box,
   Button,
   Checkbox,
+  Divider,
   FormControlLabel,
   Grid,
   Link,
   Paper,
+  Tab,
+  Tabs,
+  Typography,
   useTheme,
 } from "@mui/material";
 import { TextField2 } from "components/ui-component/customizedComponents";
@@ -15,42 +21,191 @@ import {
   updatePassword,
   reauthenticateWithCredential,
   EmailAuthProvider,
+  User,
+  updateProfile,
 } from "firebase/auth";
 import { useNavigate } from "react-router";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Person } from "@mui/icons-material";
+import { PROVINCES_AND_DISTRICTS } from "data/provinceAndDistricts";
+import { toast } from "react-toastify";
+
+interface InputsProfile {
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  province: string | null;
+  district: string | null;
+}
+
+interface InputsAccount {
+  currentPassword: string;
+  newPassword: string;
+  confirmPassword: string;
+}
+const initialValuesProfile: InputsProfile = {
+  firstName: "",
+  lastName: "",
+  phoneNumber: "",
+  province: null,
+  district: null,
+};
+
+const initialValuesAccount: InputsAccount = {
+  currentPassword: "",
+  newPassword: "",
+  confirmPassword: "",
+};
+
+const profileSchema = z.object({
+  firstName: z.string().min(1, "First Name is required"),
+  lastName: z.string().min(1, "Last Name is required"),
+  phoneNumber: z
+    .string()
+    .min(1, "Phone number is required")
+    .refine((value) => /^\d{10}$/.test(value), "Not a valid phone number"),
+  province: z.any().refine((value) => (value == null ? false : true), {
+    message: "Province is required",
+  }),
+  district: z.any().refine((value) => (value == null ? false : true), {
+    message: "District is required",
+  }),
+});
+
+const accountSchema = z
+  .object({
+    currentPassword: z.string().min(1, "Current Password is required"),
+    newPassword: z
+      .string()
+      .min(6, "Password should be at least 6 characters long")
+      .max(10, "Password should be at most 10 characters long")
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,10}$/,
+        {
+          message: "Password should follow the password guidelines",
+        }
+      ),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.newPassword === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
 
 const Profile = () => {
+  const {
+    register: registerProfileInputs,
+    watch: watchProfile,
+    control,
+    setValue,
+    getValues,
+    handleSubmit: handleSubmitProfile,
+    formState: { errors: errorsProfile },
+  } = useForm<InputsProfile>({
+    defaultValues: initialValuesProfile,
+    resolver: zodResolver(profileSchema),
+  });
+
+  const {
+    register: registerAccountInputs,
+    watch: watchAccount,
+    handleSubmit: handleSubmitAccount,
+    formState: { errors: errorsAccount },
+  } = useForm<InputsAccount>({
+    defaultValues: initialValuesAccount,
+    resolver: zodResolver(accountSchema),
+  });
+
+  const watchFirstName = watchProfile("firstName");
+  const watchLastName = watchProfile("lastName");
+  const watchPhoneNumber = watchProfile("phoneNumber");
+  const watchProvince = watchProfile("province");
+  const watchDistrict = watchProfile("district");
+
+  const watchCurrentPassword = watchAccount("currentPassword");
+  const watchNewPassword = watchAccount("newPassword");
+  const watchConfirmPassword = watchAccount("confirmPassword");
+
   const theme = useTheme();
   const navigate = useNavigate();
 
-  const [currentPassword, setCurrentPassword] = React.useState("");
-  const [newPassword, setNewPassword] = React.useState("");
-  const [confirmPassword, setConfirmPassword] = React.useState("");
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const [currentTab, setCurrentTab] = useState("1");
+  const [districtList, setDistrictList] = useState<string[]>([]);
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (newPassword !== confirmPassword) {
-      window.alert("Passwords do not match");
+  useEffect(() => {
+    if (!PROVINCES_AND_DISTRICTS || PROVINCES_AND_DISTRICTS?.length === 0)
       return;
-    }
 
-    const user = auth.currentUser;
-    if (user) {
-      const credential = EmailAuthProvider.credential(
-        user.email!,
-        currentPassword
+    if (watchProvince) {
+      const targetProvince = PROVINCES_AND_DISTRICTS.find(
+        (province) => province.province === watchProvince
       );
-      reauthenticateWithCredential(user, credential)
+      console.log(targetProvince?.districts);
+
+      setDistrictList(targetProvince?.districts || []);
+
+      // Find whether targetProvince list has the watchDistrict value.
+      // If not, set the watchDistrict to null
+      const hasDistrict = targetProvince?.districts.find(
+        (district) => district === watchDistrict
+      );
+      !hasDistrict && setValue("district", null);
+    } else {
+      setValue("district", null);
+      setDistrictList([]);
+    }
+  }, [watchProvince, setValue]);
+
+  useEffect(() => {
+    if (currentUser) {
+      setCurrentUser(auth.currentUser);
+    }
+  }, [auth]);
+
+  const onUpdateProfileDetails = (data: InputsProfile) => {
+    if (currentUser) {
+      updateProfile(currentUser, {
+        displayName: `${data.firstName} ${data.lastName}`,
+        photoURL: "",
+      })
         .then(() => {
-          updatePassword(user, newPassword)
+          console.log("Profile updated successfully");
+        })
+        .catch((error) => {
+          console.log("Error in updating profile :", error);
+        });
+    }
+  };
+
+  const onUpdatePassword = (data: InputsAccount) => {
+    if (currentUser) {
+      const credential = EmailAuthProvider.credential(
+        currentUser.email!,
+        watchCurrentPassword
+      );
+      reauthenticateWithCredential(currentUser, credential)
+        .then(() => {
+          updatePassword(currentUser, data.newPassword)
             .then(() => {
-              window.alert("Password updated successfully");
+              toast.success("Password updated successfully");
             })
             .catch((error) => {
-              window.alert(error.message);
+              console.log(error);
+              const message = error.message.split("/")[1];
+              const removeLastChar = message.slice(0, -2);
+              const finalMessage = removeLastChar.replace(/-/g, " ");
+              toast.error(finalMessage);
             });
         })
         .catch((error) => {
-          window.alert(error.message);
+          console.log(error);
+          const message = error.message.split("/")[1];
+          const removeLastChar = message.slice(0, -2);
+          const finalMessage = removeLastChar.replace(/-/g, " ");
+          toast.error(finalMessage);
         });
     }
   };
@@ -61,7 +216,7 @@ const Profile = () => {
       user
         .delete()
         .then(() => {
-          window.alert("Account deleted successfully");
+          toast.success("Account deleted successfully");
           navigate("/auth/signup");
         })
         .catch((error) => {
@@ -82,85 +237,194 @@ const Profile = () => {
 
   return (
     <>
-      <Grid container component="main" sx={{ height: "100vh" }}>
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
+      <Box>
+        <Box display={"flex"} alignItems={"center"} gap={2}>
+          <Avatar
+            alt="Remy Sharp"
+            src="/static/media/user-round.27fe79b102ea6aad2f60e66cff82818d.svg"
+            sx={{ width: 56, height: 56 }}
+          />
+          <Typography
+            variant={"h4"}
             sx={{
-              my: 8,
-              mx: 4,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}>
-            <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 1 }}>
-              <TextField2
-                size="small"
-                margin="normal"
-                required
-                fullWidth
-                id="current-password"
-                label="Current Password"
-                name="current-password"
-                autoComplete="current-password"
-                onChange={(e) => setCurrentPassword(e.target.value)}
-                autoFocus
-              />
-              <TextField2
-                size="small"
-                margin="normal"
-                required
-                fullWidth
-                name="new-password"
-                label="New Password"
-                type="new-password"
-                id="new-password"
-                onChange={(e) => setNewPassword(e.target.value)}
-                autoComplete="new-password"
-              />
-              <TextField2
-                size="small"
-                margin="normal"
-                required
-                fullWidth
-                name="confirm-password"
-                label="Confirm Password"
-                type="confirm-password"
-                id="confirm-password"
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                autoComplete="new-password"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}>
-                Change Password
-              </Button>
+              marginLeft: "10px",
+            }}
+          >
+            Hello Raj
+          </Typography>
+        </Box>
+        <Box mt={2}>
+          <Divider />
+        </Box>
+        <Box sx={{ width: "100%" }} mt={3}>
+          <Box>
+            <Tabs
+              scrollButtons="auto"
+              variant="scrollable"
+              value={currentTab}
+              onChange={(_: any, value: string) => setCurrentTab(value)}
+              aria-label="basic tabs"
+            >
+              <Tab label="Profile" value={"1"} />
+              <Tab label="Account" value={"2"} />
+            </Tabs>
+
+            <Box mt={2}>
+              {currentTab === "1" && (
+                <Paper variant="outlined" sx={{ padding: "15px" }}>
+                  <Typography variant="h6">Profile information</Typography>
+
+                  <form
+                    onSubmit={handleSubmitProfile(onUpdateProfileDetails)}
+                    style={{
+                      marginTop: "20px",
+                    }}
+                  >
+                    <Box
+                      display={"flex"}
+                      alignItems={"center"}
+                      gap={2}
+                      sx={{ mb: 2 }}
+                    >
+                      <TextField2
+                        label="First Name"
+                        sx={{ width: "300px" }}
+                        error={!!errorsProfile.firstName}
+                        helperText={errorsProfile.firstName?.message}
+                        {...registerProfileInputs("firstName")}
+                      />
+
+                      <TextField2
+                        label="Last Name"
+                        sx={{ width: "300px" }}
+                        error={!!errorsProfile.lastName}
+                        helperText={errorsProfile.lastName?.message}
+                        {...registerProfileInputs("lastName")}
+                      />
+                    </Box>
+
+                    <TextField2
+                      label="Phone Number"
+                      sx={{ mb: 2, width: "300px" }}
+                      error={!!errorsProfile.phoneNumber}
+                      helperText={errorsProfile.phoneNumber?.message}
+                      {...registerProfileInputs("phoneNumber")}
+                    />
+
+                    <Box display={"flex"} alignItems={"center"} gap={2} mb={2}>
+                      <Controller
+                        name="province"
+                        control={control}
+                        render={({ field }) => (
+                          <Autocomplete
+                            sx={{ width: "300px" }}
+                            options={PROVINCES_AND_DISTRICTS.map(
+                              (province) => province.province
+                            )}
+                            isOptionEqualToValue={(option: any, value: any) =>
+                              option === value
+                            }
+                            renderInput={(params) => (
+                              <TextField2
+                                {...params}
+                                label="Province"
+                                error={errorsProfile.province ? true : false}
+                                helperText={errorsProfile.province?.message}
+                              />
+                            )}
+                            onChange={(e, data: any) => field.onChange(data)}
+                            value={field.value}
+                          />
+                        )}
+                      />
+
+                      <Controller
+                        name="district"
+                        control={control}
+                        render={({ field }) => (
+                          <Autocomplete
+                            sx={{ width: "300px" }}
+                            options={districtList}
+                            disabled={!watchProvince}
+                            isOptionEqualToValue={(option: any, value: any) =>
+                              option === value
+                            }
+                            renderInput={(params) => (
+                              <TextField2
+                                {...params}
+                                label="District"
+                                error={errorsProfile.district ? true : false}
+                                helperText={errorsProfile.district?.message}
+                              />
+                            )}
+                            onChange={(e, data: any) => field.onChange(data)}
+                            value={field.value}
+                          />
+                        )}
+                      />
+                    </Box>
+                    <Button type="submit" variant="contained" color="primary">
+                      Update Profile
+                    </Button>
+                  </form>
+                </Paper>
+              )}
+
+              {/* Update Password */}
+              {currentTab === "2" && (
+                <Paper variant="outlined" sx={{ padding: "15px" }}>
+                  <Typography variant="h6">Account</Typography>
+
+                  <form
+                    onSubmit={handleSubmitAccount(onUpdatePassword)}
+                    style={{
+                      marginTop: "20px",
+                    }}
+                  >
+                    <Box
+                      display={"flex"}
+                      flexDirection={"column"}
+                      justifyContent={"center"}
+                      gap={2}
+                    >
+                      <TextField2
+                        label="Current Password"
+                        type="password"
+                        sx={{ width: "300px" }}
+                        error={!!errorsAccount.currentPassword}
+                        helperText={errorsAccount.currentPassword?.message}
+                        {...registerAccountInputs("currentPassword")}
+                      />
+
+                      <TextField2
+                        label="New Password"
+                        type="password"
+                        sx={{ width: "300px" }}
+                        error={!!errorsAccount.newPassword}
+                        helperText={errorsAccount.newPassword?.message}
+                        {...registerAccountInputs("newPassword")}
+                      />
+
+                      <TextField2
+                        label="Confirm Password"
+                        type="password"
+                        sx={{ mb: 2, width: "300px" }}
+                        error={!!errorsAccount.confirmPassword}
+                        helperText={errorsAccount.confirmPassword?.message}
+                        {...registerAccountInputs("confirmPassword")}
+                      />
+                    </Box>
+
+                    <Button type="submit" variant="contained" color="primary">
+                      Update Password
+                    </Button>
+                  </form>
+                </Paper>
+              )}
             </Box>
           </Box>
-        </Grid>
-      </Grid>
-
-      <Button
-        type="submit"
-        fullWidth
-        variant="contained"
-        sx={{ mt: 3, mb: 2 }}
-        onClick={handleDeleteAccount}>
-        Delete Account
-      </Button>
-      <Button
-        type="submit"
-        fullWidth
-        variant="contained"
-        sx={{ mt: 3, mb: 2 }}
-        onClick={handleLogout}>
-        Logout
-      </Button>
+        </Box>
+      </Box>
     </>
   );
 };
