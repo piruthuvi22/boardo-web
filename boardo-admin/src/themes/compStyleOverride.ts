c3edae38d3c4a64f431293cb42bcd58a13cf1c6f
Piruthuviraj <piruthuviraj22@gmail.com>
2024-04-28 16:05:41 +0530

    Textfield default props small added

--- boardo-admin/src/themes/compStyleOverride.ts
+++ boardo-admin/src/themes/compStyleOverride.ts
@@ -85,47 +85,6 @@ export default function componentStyleOverrides(theme: any): Components {
         },
       },
     },
-    MuiInputBase: {
-      styleOverrides: {
-        input: {
-          color: theme.textDark,
-          "&::placeholder": {
-            color: theme.darkTextSecondary,
-            fontSize: "0.875rem",
-          },
-        },
-      },
-    },
-    MuiOutlinedInput: {
-      styleOverrides: {
-        root: {
-          background: bgColor,
-          "& .MuiOutlinedInput-notchedOutline": {
-            borderColor: theme.colors?.grey400,
-          },
-          "&:hover $notchedOutline": {
-            borderColor: theme.colors?.primaryLight,
-          },
-          "&.MuiInputBase-multiline": {
-            padding: 1,
-          },
-        },
-        input: {
-          fontWeight: 500,
-          background: bgColor,
-          padding: "15.5px 14px",
-          "&.MuiInputBase-inputSizeSmall": {
-            padding: "10px 14px",
-            "&.MuiInputBase-inputAdornedStart": {
-              paddingLeft: 0,
-            },
-          },
-        },
-        inputAdornedStart: {
-          paddingLeft: 4,
-        },
-      },
-    },
     MuiSlider: {
       styleOverrides: {
         root: {
